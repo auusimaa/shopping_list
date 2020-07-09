3abeff6a92e7b09434fc776630b18ac041c7dc6a
Arttu Uusimaa <arttu1012@gmail.com>
2020-07-09 18:02:59 +0300

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -15,7 +16,6 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 //Connect to mongo
-
 mongoose
     .connect(db, {
         useNewUrlParser: true,
@@ -25,8 +25,17 @@ mongoose
     .catch(err => console.log(err));
 
 // Use Routes
-
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if( process.env.NODE_ENV === 'production' ) {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
